b0755693161343b08d62d405ac6604871f280af1
scottklein7 <scott.klein02@protonmail.com>
2022-06-09 17:31:53 -0400

    WIP: basic design of merchant page

--- src/components/PrevTips.js/index.js
+++ src/components/PrevTips.js/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from './styles.module.css'
 import React from 'react'
+import PlebQRCode from '@/components/PlebQRCode'
 
 function index() {
     return (
@@ -9,24 +10,27 @@ function index() {
             <Head>
                 <title></title>
             </Head>
-            <main className={styles.main}>
-                <div>
-                    <h1>Resturant Title</h1>
+            <main>
+                <div className={styles.divtext}>
+                    <h1 className={styles.tipeh1}>Resturant Title</h1>
                     <p>This is the resurant info the will be in the desc</p>
                 </div>
 
-                <div>
-                    {/* qr code ln */}
-                    
+                <div className={styles.qrcode}>
+                    <PlebQRCode />
                 </div>
 
-                {/* tips */}
-
-                <div>
-                    <h3>Prior Tips</h3>
-                    <div>
-                        <span>Amount - 100 sats</span>
-                        <span>Note - this is a tipsß</span>
+                <div className={styles.prevtipsmdiv}>
+                    <h3 className={styles.priortipsh3}>Prior Tips</h3>
+                    <div className={styles.transInfo}>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Amount</span> - 100 sats</p>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Date</span> - 100 sats</p>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Note</span> - 100 sats</p>
+                    </div>
+                    <div className={styles.transInfo}>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Amount</span> - 100 sats</p>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Amount</span> - 100 sats</p>
+                        <p className={styles.transpinfo}><span className={styles.transpan}>Amount</span> - 100 sats</p>
                     </div>
                 </div>
 
