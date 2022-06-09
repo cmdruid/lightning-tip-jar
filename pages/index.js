b96da16e579e6834ffcc246d7c1822b756182e0c
cmd <cmd@yggdrasil.onion>
2022-06-09 12:17:30 -0500

    added alias for styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
 import LoginQrCode from '@/components/LoginQrCode'
 import Landing from '@/components/Landing/index.js'
