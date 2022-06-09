dc80a71b9f137c00c7c82785b66e877ddf138b5a
Will Sutton <wsutton17@gmail.com>
2022-06-09 17:37:55 -0400

    basic qr + recent tips

--- src/components/PlebQRCode/index.js
+++ src/components/PlebQRCode/index.js
@@ -1,6 +1,8 @@
 import useSWR from 'swr'
-
 import Image  from 'next/image'
+import { useState } from 'react'
+import useClipboard from "react-use-clipboard";
+
 // import styles from './styles.module.css'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
@@ -8,14 +10,31 @@ const fetcher = (...args) => fetch(...args).then(res => res.json())
 export default function PlebQrCode() {
 
     const { data, error } = useSWR('/api/merch-lnp/get-lnp', fetcher)
+
+    const [isCopied, setCopied] = useClipboard(
+      data ? data.lnUrl : "", 
+      {successDuration: 1000},
+    )
   
-  if (error) return <div>failed to load!</div>
+  if (error) return <div>could not load recent payments</div>
   if (!data) return <div>loading...</div>
-  if (!data.image) return <div>Unable to fetch QR code!</div>
+  if (!data.image) return <div>Unable to fetch recent payments</div>
 
   return (
-    <div className="yoo">
+    <div className="payment-container">
+      
       <Image src={data.image} alt="Login Code" width={300} height={300} />
+      
+      {/* <p>{data.lnUrl}</p> */}
+      
+      <div className="copy-lnurl-container">
+      
+        <button onClick={setCopied} style={{"margin": "10px"}}>
+          {isCopied ? "Copied!" : "Copy LN-URL"}
+        </button>        
+      
+      </div>
+    
     </div>
   )
 }
