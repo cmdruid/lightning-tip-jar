2bd53866efed858b961728f4cfd3c1176e147ede
Will Sutton <wsutton17@gmail.com>
2022-06-09 17:37:55 -0400

    basic qr + recent tips

--- pages/shiners.js
+++ pages/shiners.js
@@ -3,21 +3,25 @@
 // import Link from 'next/link'
 
 import PlebQRCode from '@/components/PlebQRCode'
+import PlebRecentTips from '@/components/PlebRecentTips'
 // import Landing from 'components/Landing/index.js'
 // import { FaGithub } from 'react-icons/fa'
 
 export default function Cars() {
   return (
-    <div className={"dummy"}>
+    <div className={"dummy"} style={{"background-color": "#ffffff"}}>
       {/* <Link href="/cars"> Click Me</Link> */}
       <h2>Shiners</h2>
       <PlebQRCode/>
-      <h2>Admin Panel</h2>
+      
+      <PlebRecentTips/>
+      
+      {/* <h2>Admin Panel</h2>
       <form action="/">
           <label>LN-URL</label>
           <input type="text" placeholder="LN-URL" />
           <input type="submit"/>
-      </form>
+      </form> */}
     </div>
   )
 }
