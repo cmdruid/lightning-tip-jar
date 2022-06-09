062d70ba5b7cddb94ffabdb98b3b7efc9df9809f
cmd <cmd@yggdrasil.onion>
2022-06-09 11:56:11 -0500

    added skeleton for project

--- src/components/TipCode/index.js
+++ src/components/TipCode/index.js
@@ -1,20 +1,19 @@
 import useSWR from 'swr'
-
 import Image  from 'next/image'
 import styles from './styles.module.css'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export default function OfferCode() {
+export default function TipComponent() {
   const { data, error } = useSWR('/api/getoffer', fetcher)
   
   if (error) return <div>failed to load!</div>
   if (!data) return <div>loading...</div>
-  if (!data.qrcode) return <div>Unable to locate offer!</div>
+  if (data && !data.item) return <div>Unable to locate item!</div>
 
   return (
-    <div className={styles.qrcode}>
-      <Image src={data.qrcode} alt="BOLT12 QR Code" width={300} height={300} />
+    <div className={styles.div}>
+      <p>Filler</p>
     </div>
   )
 }
