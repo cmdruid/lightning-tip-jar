e62c906d4bdea64477b99d5c990fc4970cd91bc0
scottklein7 <scott.klein02@protonmail.com>
2022-06-09 15:34:02 -0400

    Wip: add starting api

---
+++ pages/tipme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PrevTips from '@/components/PrevTips.js/index'
+
+function tipme() {
+  return (
+    <div>
+        <PrevTips />
+    </div>
+  )
+}
+
+export default tipme
