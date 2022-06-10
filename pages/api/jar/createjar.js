d00beb2e1da219b7d8aac3965391bfa4c45ac065
cmd <cmd@yggdrasil.onion>
2022-06-09 19:51:34 -0500

    topher

--- pages/api/jar/createjar.js
+++ pages/api/jar/createjar.js
@@ -1,14 +1,12 @@
 import { getCollection } from "@/lib/controller";
 import { JarModel }      from '@/model/jar'
-
-import { nanoid }         from 'nanoid'
-import { string, object } from 'yup'
+// import { string, object } from 'yup'
 import { errorHandler }   from '@/lib/error'
 
-const schema = object().shape({
-  slug : string().trim().matches(/^[\w\-]+$/i),
-  url  : string().trim().url().required()
-});
+// const schema = object().shape({
+//   slug : string().trim().matches(/^[\w\-]+$/i),
+//   url  : string().trim().url().required()
+// });
 
 export default async function createJar(req, res) {
 
@@ -17,6 +15,8 @@ export default async function createJar(req, res) {
 
   // Grab the slug and url from the post body.
   let { slug, title, description, ...opts } = req.body;
+
+  console.log(req.body);
 
   try {
     // Validate our inputs!
@@ -30,14 +30,14 @@ export default async function createJar(req, res) {
     // Generate unique url
 
     // Fetches the collection, and checks if the slug exists.
-    const urls   = await getCollection(JarModel),
-          exists = await urls.findOne({ slug });
-    if (exists) return res.status(409).end();
+    // const jars   = await getCollection(JarModel),
+    //       exists = await urls.findOne({ slug });
+    // if (exists) return res.status(409).end();
 
-    // Insert new slug and URL into the collection.
-    const created = await urls.insertOne({ slug, url });
-    if (!created) throw new Error('No response from db.');
+    // // Insert new slug and URL into the collection.
+    // const created = await jars.insertOne({ slug, url });
+    // if (!created) throw new Error('No response from db.');
 
-    return res.status(200).end();
+    return res.status(200).json({ok: true});
   } catch(err) { errorHandler(req, res, err) }
 }
