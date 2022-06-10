ecfce8dc7e66015454e790c85bebfe96bce18f34
cmd <cmd@yggdrasil.onion>
2022-06-10 10:57:28 -0500

    fixed bug with getTransactions

--- pages/api/user/getTransactions.js
+++ pages/api/user/getTransactions.js
@@ -6,9 +6,9 @@ export default async function getTransactions(req, res) {
   if (req.method !== 'GET') res.status(400).end();
 
   // Grab the slug and url from the post body.
-  let { invoiceKey } = req.query;
+  let { walletKey } = req.query;
 
-  if (!invoiceKey) res.status(400).end();
+  if (!walletKey) res.status(400).end();
 
   return listPayments(walletKey)
 }
