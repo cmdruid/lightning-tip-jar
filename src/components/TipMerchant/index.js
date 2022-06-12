f46ba8dfe080bdd0b13c840c80428d85a1fff22f
austinkelsay <austinkelsay@yahoo.com>
2022-06-12 13:10:15 -0500

    Added floating home button, fixed call to action icon spacing, fixed up prev tips styles, added spacing around qrcode

--- src/components/TipMerchant/index.js
+++ src/components/TipMerchant/index.js
@@ -3,12 +3,14 @@ import PrevTips from '@/components/PrevTips/index.js'
 import PlebQRCode from '@/components/PlebQrCode'
 import PrevTipHeader from '@/components/PrevTipHeader'
 import Footer from '@/components/Footer/index.js'
+import HomeButton from '@/components/HomeButton/index.js'
 // import useSWR from 'swr'
 
 function TipMe({user}) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <HomeButton />
         <PrevTipHeader
           title={user.title}
           desc={user.description}
