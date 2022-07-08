c4cd63fdd7f5fd101e55ec9eaf88ffe96fe93ed4
C Scott <cscottdev@protonmail.com>
2022-07-08 14:28:28 -0500

    slight refactor

--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import * as crypto from '@/lib/crypto';
 
-describe('lib/crypto/hash', () => {
+describe('crypto.hash', () => {
   it('Hashes a string using SHA-256', async () => {
     const testString  = '4e4de6c3-e101-48d8-913b-ef80a115da5f',
           correctHash = '931d5ec90d6387e88346729f5c0c12545a4c9a3ed6429817fbd3c28fda717614',
